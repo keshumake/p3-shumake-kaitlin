e331c3da0c7c4357a090bf4c747cafc081a937db
Kaitlin Shumake <shumakek@ufl.edu>
2021-08-02 22:12:53 -0400

    Updated php forms

--- form/default.inc.php
+++ form/default.inc.php
@@ -7,61 +7,70 @@
 }
 
 main {
-    padding: 25px 150px;
-    background-color: antiquewhite;
-    min-height: 100vh;
+  min-height: 100vh;
+  padding: 50px 0px;
 }
 
-header {
-  background-color: #555;
-  padding: 10px;
-  text-align: center;
-  margin: 0 auto;
+.main-header {
+  height: 100px;
+  width: 100%;
+  background-color: #e0d9cd;
+  display: inline-block;
 }
 
-nav {
-  padding: 25px;
+.top-header h1, .main-header h1 {
+    padding-left: 25px;
+    border: 0;
+    width: 30%;
+    display: inline-block;
+}
+
+header nav, .main-header nav {
+    display: inline-block;
+    width: 60%;
+}
+
+header ul, .main-header ul {
+  margin: 0;
+  padding: 0;
+  text-align: right;
 }
 
 header nav li {
+  font-family: 'Noto Sans JP', sans-serif;
   display: inline;
-  text-decoration: none;
+  list-style: none;
+}
+
+header nav li a {
+  display: inline;
   padding: 10px;
-  padding-bottom: 0;
-  font-color: white;
-  font-size: 20px;
-  letter-spacing: 3px;
-}
-
-header nav li a:link, nav li a:visited {
   text-decoration: none;
-  color: white;
-  border-bottom: 2pt solid white;
+  color: black;
+  letter-spacing: 2px;
 }
 
-h3 {
-    font-size: 20;
-    font-family: 'Georgia', serif;
-    text-align: center;
-}
-
-footer {
-  position: relative;
-  padding: 25px 0;
-  background-color: #555;
-  text-align: center;
-  width: 100%;
+header nav li a:hover {
+  background-color: #aac991;
   color: white;
 }
 
-.container {
-  max-width: 900px;
-  margin: 50px auto 180px;
-  padding: 20px 0;
+header nav li a:visited {
+  color: black;
+}
+
+h1, h2, h3 {
+  font-family: 'Homemade Apple', cursive;
+}
+
+p {
+  font-family: 'Noto Sans JP', sans-serif;
+  line-height: 16pt;
+  font-size: 12pt;
 }
 </style>
 
-<header>
+<header class="main-header">
     <nav>
       <ul>
         <li><a href="index.html">home</a></li>
